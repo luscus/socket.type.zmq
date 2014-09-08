c57bf1ed2852b7f15c40929b359a75ae1a5a8828
luscus <luscus.redbeard@gmail.com>
2014-09-08 18:43:50 +0200

    edited - not working

--- test/test.js
+++ test/test.js
@@ -1,1 +1,20 @@
 // testing
+var Socket = require('../../service.lib.socket/lib/Socket'),
+    zmqLib = require('../lib/socket.type.zmq'),
+    service = {
+      name: 'zmqTest'
+    },
+    client = {
+      name: 'client',
+      type: 'zmq',
+      protocol: 'tcp',
+      pattern: 'requester'
+    },
+    server = {};
+
+
+
+var socket = new Socket(client);
+
+zmqLib(socket);
+console.log('result',socket);
